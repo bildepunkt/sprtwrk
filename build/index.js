aa84dae5a4e27a167ba663a9e8597f8fceaf6c65
Chris Peters <bildepunkt@gmail.com>
2017-03-07 10:39:52 -0800

    adjust ts build params

--- build/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Foo_1 = require("./pkg/Foo");
-function greeter(person) {
-    var foo = new Foo_1.default("baz");
-    return "Hello " + person.fname + " " + person.lname;
-}
-console.log(greeter({ fname: "Diego", lname: "Sanchez" }));
--- build/pkg/Foo.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Foo = (function () {
-    function Foo(bar) {
-        this.bar = bar;
-    }
-    return Foo;
-}());
-exports.default = Foo;
