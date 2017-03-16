fc46db2002cdd397853e712b5b3aa71ff2667c39
Chris Peters <bildepunkt@gmail.com>
2017-03-16 01:03:34 -0700

    add Tree#layersEach

--- src/Tree.ts
+++ src/Tree.ts
@@ -1,3 +1,4 @@
+import Display from "./Display";
 import Layer from "./Layer";
 
 /**
@@ -23,6 +24,34 @@ export default class Tree {
   constructor (bgColor : string, ...layers : Array<Layer>) {
     this.bgColor = bgColor;
     this._layers = layers;
+  }
+
+  /**
+   * Applies the supplied function to each item in every layer
+   * @param {(item:Display, i:number, layer:Layer)=>boolean} fn - return false to short-circuit the entire loop
+   * @param {object} [scope]
+   */
+  public layersEach (fn : (item:Display, i:number, layer:Layer)=>boolean, scope : any) : void {
+    let doContinue : boolean = true;
+
+    for (let l = 0, layersLen = this._layers.length; l < layersLen; l++) {
+      const layer : Layer = this._layers[l];
+
+      if (doContinue === false) {
+        break;
+      }
+
+      for (let i = 0, itemsLen = layer.items.length; i < itemsLen; i++) {
+        const item : Display = layer[i];
+
+        fn = scope ? fn.bind(scope) : fn;
+        doContinue = fn(item, i, layer);
+
+        if (doContinue === false) {
+          break;
+        }
+      }
+    }
   }
 
   public get layers () : Array<Layer> {
