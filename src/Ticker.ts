ed911021c87a7d8c8279dee9ea80e6bb9cbc0294
Chris Peters <cpeters@pingidentity.com>
2017-03-27 13:06:21 -0700

    add Ticker

---
+++ src/Ticker.ts
@@ -0,0 +1,38 @@
+export default class Ticker {
+
+  private callback: (delta: number) => undefined;
+  private canTick: boolean = true;
+  private maxDelta: number = 32;
+  private reqId: number;
+  private then: number;
+
+  constructor (callback) {
+    this.callback = callback;
+    this.tick = this.tick.bind(this);
+    this.then = Date.now();
+  }
+
+  public tick (): void {
+    if (!this.canTick) {
+      return;
+    }
+
+    const now: number = Date.now();
+    // cap delta so things don't go haywire on "blur"
+    const delta: number = Math.min(now - this.then, this.maxDelta);
+
+    this.callback(delta);
+    this.then = now;
+
+    this.reqId = requestAnimationFrame(this.tick);
+  }
+
+  public start (): void {
+    this.reqId = requestAnimationFrame(this.tick);
+  }
+
+  public stop (): void {
+    this.canTick = false;
+    cancelAnimationFrame(this.reqId);
+  }
+}
---
+++ test/TickerTest.ts
@@ -0,0 +1,37 @@
+import Ticker from "../src/Ticker";
+import { expect } from "chai";
+import { spy } from "sinon";
+import "mocha";
+
+declare const global: Window;
+
+const manualRafTick = (): void => {
+  rafCallback();
+}
+let rafCallback;
+let rafId = 0;
+
+global.requestAnimationFrame = function (callback: Function): number {
+  rafCallback = callback;
+  return rafId++;
+};
+global.cancelAnimationFrame = function (id: number): void {
+  rafCallback = null;
+  rafId = 0;
+};
+
+describe("Ticker", () => {
+  let cb, ticker;
+
+  beforeEach(() => {
+    cb = spy();
+    ticker = new Ticker(cb);
+  });
+
+  it("executes callback on raf tick", () => {
+    ticker.start();
+    manualRafTick();
+    expect(cb.calledOnce).to.be.true;
+    expect(cb.calledWith(0)).to.be.true;
+  });
+});
